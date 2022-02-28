8e4b24c7c99ca895fcf3150eb671c2c17c13c352
iGalaxyYT <contact@igalaxy.dev>
2022-02-27 23:24:19 -0800

    fix: future-proof cards limit

--- pages/api/github/[owner]/[repo]/[project]/column.ts
+++ pages/api/github/[owner]/[repo]/[project]/column.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { GitHubProject, GitHubProjectColumn } from 'lib/types';
+import {
+	GitHubProject,
+	GitHubProjectColumn,
+	GitHubProjectColumnCard,
+} from 'lib/types';
 
 export default async function handler(
 	req: NextApiRequest,
@@ -19,13 +23,23 @@ export default async function handler(
 		{ headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` } }
 	).then(res => res.json());
 
-	return res.json(
-		await fetch(
+	const cards: GitHubProjectColumnCard[] = [];
+
+	async function fetchCards(page: number) {
+		const newCards = await fetch(
 			columnsResponse.find((x: GitHubProjectColumn) => x.name === column)
-				.cards_url + '?per_page=100',
+				.cards_url + `?per_page=100&page=${page}`,
 			{
 				headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` },
 			}
-		).then(res => res.json())
-	);
+		).then(res => res.json());
+
+		cards.push(...newCards);
+
+		if (newCards.length === 100) await fetchCards(page + 1);
+	}
+
+	await fetchCards(1);
+
+	return res.json(cards);
 }
