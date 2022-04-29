17b029700437a8d49c6135cc672c5c7270e7f3c5
iGalaxyYT <contact@igalaxy.dev>
2022-04-29 13:28:40 -0700

    misc: use multiple minecraft playtime sources

--- lib/hooks/minecraft.ts
+++ lib/hooks/minecraft.ts
@@ -3,5 +3,9 @@ import { useLanyard } from 'use-lanyard';
 export function useMinecraftPlaytime() {
 	const { data } = useLanyard('182292736790102017');
 
-	return +`${data?.kv.polymc_playtime}`;
+	return (
+		Number(data?.kv.polymc_playtime) +
+		Number(data?.kv.polymc_playtime_macbook) +
+		Number(data?.kv.polymc_playtime_steamdeck)
+	);
 }
